9e9ab457b1e3dcdf7bcf560aa896b5e5768e844a
Chris Hardin <starjumper30@gmail.com>
2024-03-01 16:55:50 -0600

    replace flask templates with angular app

--- app.py
+++ app.py
@@ -8,25 +8,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'index.html')
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    name = request.args.get('name')
+    return {'name': name}
+
+
+@app.route('/api/hello', methods=['POST'])
+def hello():
+    name = request.form.get('name')
+    return {'name': name}
+
+
+@app.route('/<path:path>')
+def static_assets(path):
+    return send_from_directory(os.path.join(app.root_path, 'static'), path)
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
